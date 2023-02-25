0dd9dd83cfdc25697a861183989487dc28c1edd9
BasileKoffiDataScientist <koffi_basile@yahoo.fr>
2023-02-25 00:29:09 -0500

    all problems were fixed

--- cvApp/urls.py
+++ cvApp/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('contact', views.contact_view,name="contact"),
     path('resume', views.resume_view,name="resume"),
     path('about', views.about_view,name="about"),
+    path('download_cv/', views.download_cv, name='download_cv'),
 
 ]
--- cvApp/views.py
+++ cvApp/views.py
@@ -1,6 +1,37 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import FileResponse
+from django.conf import settings
+import os
+
+import os
+from django.conf import settings
+from django.http import FileResponse
+
+
+def download_cv(request):
+    # Obtenez le chemin d'accès complet au fichier PDF
+    cv_path = os.path.join(settings.MEDIA_ROOT, 'CV_BasileKoffi_DataScientist_en.pdf')
+
+    # Ouvrez le fichier PDF en mode binaire
+    cv_file = open(cv_path, 'rb')
+
+    # Créez une réponse HTTP en utilisant la méthode FileResponse
+    response = FileResponse(cv_file, content_type='application/pdf')
+
+    # Définissez le nom de fichier pour la réponse
+    response['Content-Disposition'] = 'attachment; filename="cv_BasileKoffi.pdf"'
+
+    return response
+
+
+# def download_cv(request):
+#     cv_path = os.path.join(settings.BASE_DIR, 'path', 'to', 'cv.pdf')
+#     cv_file = open(cv_path, 'rb')
+#     response = FileResponse(cv_file, content_type='application/pdf')
+#     response['Content-Disposition'] = 'attachment; filename="cv.pdf"'
+#     return response
 
 
 def index_view(request):
